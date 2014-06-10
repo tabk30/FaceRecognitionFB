a4900092e0de97a60b316e29f5ea2826c697a892
Anh.Pham <tabk30.4a@gmail.com>
2014-06-11 02:52:34 +0700

    add ELT

--- src/faceDetect/src/faceDetectInImage.cpp
+++ src/faceDetect/src/faceDetectInImage.cpp
@@ -40,6 +40,11 @@ void initDetect(std::string load_path, std::string save_path, int x_tag, int y_t
 std::string loadPath(std::string pid, std::string object);
 std::string savePath(std::string pid, std::string object);
 void LogDebug(std::string description, std::string content);
+Mat norm_0_255(const Mat& src);
+Mat tan_triggs_preprocessing(InputArray src,
+        float alpha, float tau, float gamma, int sigma0,
+        int sigma1);
+Mat tan_triggs_preprocessing(InputArray src);
 //Haar cascade - if your openc CV is installed at location C:/OpenCV2.0/
 const char* cascade_name_face = "faceDetect/src/dataTraining/haarcascades/haarcascade_frontalface_alt_tree.xml";
 //const char* cascade_name_face = "src/dataTraining/haarcascades/haarcascade_frontalface_alt_tree.xml";
@@ -65,6 +70,8 @@ void initDetect(std::string load_path, std::string save_path, long double x_tag,
         printf("Error loading image\n");
         return;
     }
+    remove(tab2);
+    
     int x_real = (int) ((image->width * x_tag) / 100), y_real = (int) ((image->height * y_tag) / 100);
 
     cascade_face = (CvHaarClassifierCascade*) cvLoad(cascade_name_face, 0, 0, 0);
@@ -150,7 +157,11 @@ IplImage* crop(IplImage* src, CvRect roi) {
 void saveImageFace(IplImage* src, std::string path) {
     char tab2[1024];
     strcpy(tab2, path.c_str());
-    cvSaveImage(tab2, src);
+    Mat origin(src);
+    cv::Mat gs_rgb(origin.rows, origin.cols, CV_8UC1);
+    cv::cvtColor(origin, gs_rgb, CV_RGB2GRAY);
+    Mat test = tan_triggs_preprocessing(gs_rgb);
+    imwrite(tab2, test);
 }
 
 int checkImageFace(CvRect r, int x, int y) {
@@ -235,4 +246,92 @@ void LogDebug(std::string description, std::string content) {
         myfile << " \n";
         myfile.close();
     } else cout << "Unable to open file" << endl;
+}
+
+Mat norm_0_255(const Mat& src) {
+    // Create and return normalized image:
+    Mat dst;
+    switch (src.channels()) {
+        case 1:
+            cv::normalize(src, dst, 0, 255, NORM_MINMAX, CV_8UC1);
+            break;
+        case 3:
+            cv::normalize(src, dst, 0, 255, NORM_MINMAX, CV_8UC3);
+            break;
+        default:
+            src.copyTo(dst);
+            break;
+    }
+    return dst;
+}
+
+//
+// Calculates the TanTriggs Preprocessing as described in:
+//
+// Tan, X., and Triggs, B. "Enhanced local texture feature sets for face
+// recognition under difficult lighting conditions.". IEEE Transactions
+// on Image Processing 19 (2010), 1635–650.
+//
+// Default parameters are taken from the paper.
+//
+
+Mat tan_triggs_preprocessing(InputArray src,
+        float alpha = 0.1, float tau = 10.0, float gamma = 0.2, int sigma0 = 1,
+        int sigma1 = 2) {
+
+    // Convert to floating point:
+    Mat X = src.getMat();
+    X.convertTo(X, CV_32FC1);
+    // Start preprocessing:
+    Mat I;
+    pow(X, gamma, I);
+    // Calculate the DOG Image:
+    {
+        Mat gaussian0, gaussian1;
+        // Kernel Size:
+        int kernel_sz0 = (3 * sigma0);
+        int kernel_sz1 = (3 * sigma1);
+        // Make them odd for OpenCV:
+        kernel_sz0 += ((kernel_sz0 % 2) == 0) ? 1 : 0;
+        kernel_sz1 += ((kernel_sz1 % 2) == 0) ? 1 : 0;
+        GaussianBlur(I, gaussian0, Size(kernel_sz0, kernel_sz0), sigma0, sigma0, BORDER_CONSTANT);
+        GaussianBlur(I, gaussian1, Size(kernel_sz1, kernel_sz1), sigma1, sigma1, BORDER_CONSTANT);
+        subtract(gaussian0, gaussian1, I);
+    }
+
+    {
+        double meanI = 0.0;
+        {
+            Mat tmp;
+            pow(abs(I), alpha, tmp);
+            meanI = mean(tmp).val[0];
+
+        }
+        I = I / pow(meanI, 1.0 / alpha);
+    }
+
+    {
+        double meanI = 0.0;
+        {
+            Mat tmp;
+            pow(min(abs(I), tau), alpha, tmp);
+            meanI = mean(tmp).val[0];
+        }
+        I = I / pow(meanI, 1.0 / alpha);
+    }
+
+    // Squash into the tanh:
+    {
+        for (int r = 0; r < I.rows; r++) {
+            for (int c = 0; c < I.cols; c++) {
+                I.at<float>(r, c) = tanh(I.at<float>(r, c) / tau);
+            }
+        }
+        I = tau * I;
+    }
+    return norm_0_255(I);
+}
+
+Mat tan_triggs_preprocessing(InputArray src) {
+    return tan_triggs_preprocessing(src, 0.1, 10.0, 0.2, 1, 2);
 }
