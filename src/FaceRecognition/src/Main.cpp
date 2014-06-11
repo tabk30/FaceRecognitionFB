9dce66cd7195ac55bd81d1072ccff04892a91b29
Anh.Pham <tabk30.4a@gmail.com>
2014-06-12 03:15:48 +0700

    fix argc recognition

--- src/FaceRecognition/src/Main.cpp
+++ src/FaceRecognition/src/Main.cpp
@@ -11,12 +11,12 @@ using namespace cv;
 using namespace std;
 
 int main(int argc, const char *argv[]) {
-    if(argc < 2){
+    if(argc < 3){
          cout << "Not enough data" << endl;
          return 1;
     }
     FaceRecogniontPCA * pca_test = new FaceRecogniontPCA();
-    string name(argv[0]);
     string path(argv[1]);
+    string name(argv[2]);
     pca_test->recognition(path, name);
 }
